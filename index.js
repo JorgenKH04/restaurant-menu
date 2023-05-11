d303ee68d59dbea92f789f9b99a97401e1ef6653
JorgenKH04 <131149458+JorgenKH04@users.noreply.github.com>
2023-05-11 14:54:13 +0200

    Update index.html and index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("pls work");
+import { menuArray } from "./data.js";
+
+function renderMenu(itemArray) {
+  const menuHTML = "";
+  itemArray.forEach((menuItem) => {
+    menuHTML += `
+    <div class="icon-menu">
+        <p class="icons">🍕</p>
+        <p>
+            <span class="food-span">Pizza</span>
+            <span class="food-ingredients">pepperoni,mushrom,mozarella</span>$14
+        </p>
+        <button>+</button>
+    </div>;`;
+  });
+}
